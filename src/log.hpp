967398347db5c10e7fe81a20d503baf8c31d4c5c
nyorain <nyorain@gmail.com>
2015-07-17 18:01:55 +0200

    there is now a stream/function for debug information(log), warnings, and errors, which are at default some custom fstream (log), std::cout and std::cerr

--- src/log.hpp
+++ src/log.hpp
@@ -1,5 +1,31 @@
 #pragma once
 
 #include <ostream>
+#include <util/misc.hpp>
 
-extern std::ostream& iroLog;
+////////////
+extern std::ostream* warningStream;
+extern std::ostream* debugStream;
+extern std::ostream* errorStream;
+
+template<typename ... Args> void iroDebug(Args ... args)
+{
+    #ifdef IRO_DEBUG
+        printVars(*debugStream, args ...);
+        *debugStream << std::endl;
+    #endif // IRO_DEBUG
+}
+
+template<typename ... Args> void iroWarning(Args ... args)
+{
+    printVars(*warningStream, "warning: ", args ...);
+    *warningStream << std::endl;
+}
+
+
+template<typename ... Args> void iroError(Args ... args)
+{
+    printVars(*errorStream, "error: ", args ...);
+    *errorStream << std::endl;
+}
+
